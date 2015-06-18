0e4c2e56eb40ed2b4873605eb869cfb1f5a9c6b2
cfc <cfc@ufrj.br>
2015-06-18 14:55:05 -0300

    Commit Zero

---
+++ SistemaVendas/SistemaVendas/CalculadoraComissao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemaVendas
+{
+    public static class CalculadoraComissao
+    {
+        private static readonly double PorcentagemComissaoVendaBaixa = 0.05;
+
+        private static double ArredondarComissao(double comissao)
+        {
+            return Math.Floor(comissao * 100) / 100;
+        }
+
+        public static double Calcular(double valorVenda)
+        {
+            double comissao = valorVenda * PorcentagemComissaoVendaBaixa;
+            return ArredondarComissao(comissao);
+        }
+    }
+}
---
+++ SistemaVendas/TesteVendas/TesteCalculoComissao.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaVendas;
+
+namespace TesteVendas
+{
+    [TestClass]
+    public class TesteCalculoComissao
+    {
+        [TestMethod]
+        public void Valor_inteiro_menor_de_10000_retorna_5_porcento()
+        {
+            int valorVenda = 1000;
+            int valorEsperado = 50;
+            double retorno = 0;
+
+            retorno = CalculadoraComissao.Calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+
+        [TestMethod]
+        public void Valor_igual_10000_retorna_5_porcento()
+        {
+            int valorVenda = 10000;
+            int valorEsperado = 500;
+            double retorno = 0;
+
+            retorno = CalculadoraComissao.Calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+
+        [TestMethod]
+        public void Valor_real_menor_de_10000_retorna_5_porcento_arredondado()
+        {
+            double valorVenda = 1.99;
+            double valorEsperado = 0.09;
+            double retorno = 0;
+
+            retorno = CalculadoraComissao.Calcular(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+    }
+}
