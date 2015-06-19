2129a8671965ae04e4791cda0643dd07bb393399
cfc <cfc@ufrj.br>
2015-06-19 11:42:23 -0300

    codigo do segundo dojo, pre almoco

---
+++ SistemaVendas/SistemaVendas/CalculadoraRoyalties.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SistemaVendas;
+
+namespace TesteVendas
+{
+    public class CalculadoraRoyalties
+    {
+        IRepositorioVendas repVendas;
+        public CalculadoraRoyalties(IRepositorioVendas repVendas)
+        {
+            this.repVendas = repVendas;
+        }
+
+        public double Calcular(int mes, int ano)
+        {
+            List<double> valoresVendas = repVendas.GetVendas(mes, ano);
+
+            double totalVendaLiquida = 0;
+
+            for (int i = 0; i < valoresVendas.Count; i++)
+            {
+                double comissao = CalculadoraComissao.Calcular(valoresVendas[i]);
+                totalVendaLiquida += (valoresVendas[i] -  comissao ) ;
+            }
+
+            
+            return totalVendaLiquida * 0.2 ;
+        }
+
+
+    }
+
+}
---
+++ SistemaVendas/TesteVendas/RoyaltiesTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SistemaVendas;
+using TesteVendas;
+
+
+namespace TesteVendas
+{
+    [TestClass]
+    public class RoyaltiesTest
+    {
+
+        private Mock<IRepositorioVendas> mockRepository = null;
+        private CalculadoraRoyalties calculadoraRoyalties = null;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            mockRepository = new Mock<IRepositorioVendas>();
+            calculadoraRoyalties = new CalculadoraRoyalties(mockRepository.Object);
+        }
+
+        [TestMethod]
+        public void Calculo_Royalties_com_Valor_No_Mes_Zerado()
+        {
+            double ValorRoyaltiesEsperado = 0;
+            int mes = 3;
+            int ano = 2015;
+        
+            mockRepository
+               .Setup(repositorioVenda => repositorioVenda.GetVendas(mes, ano))
+               .Returns(new List<double>() );
+
+            double totalRoyalties = calculadoraRoyalties.Calcular(mes, ano);
+
+            Assert.AreEqual(ValorRoyaltiesEsperado, totalRoyalties );
+
+        }
+
+        [TestMethod]
+        public void Calculo_Royalties_com_Valor_No_Mes_um()
+        {
+            double ValorRoyaltiesEsperado = 19;
+            int valor = 100;
+            int mes = 1;
+            int ano = 2015;
+        
+            mockRepository
+               .Setup(repositorioVenda => repositorioVenda.GetVendas(mes, ano))
+               .Returns(new List<double>() { valor });
+
+            double totalRoyalties = calculadoraRoyalties.Calcular(mes, ano);
+
+            Assert.AreEqual(ValorRoyaltiesEsperado, totalRoyalties);
+
+
+        }
+    }
+}
